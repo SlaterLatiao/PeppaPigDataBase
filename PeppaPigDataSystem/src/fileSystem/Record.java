b2bacef2e9d2ceb5e868f210a8b9f812b3d33240
Jinru Shi <qwe552325145@163.com>
2018-07-25 12:15:17 -0500

    page update 07/24

--- PeppaPigDataSystem/src/fileSystem/Record.java
+++ PeppaPigDataSystem/src/fileSystem/Record.java
@@ -1,6 +1,6 @@
 package fileSystem;
 import java.util.ArrayList;
-
+import Common.DataType;
 
 /**
  * @author Jinru Shi & Li Liu 2018-07-23
@@ -15,8 +15,9 @@ public class Record {
     private ArrayList<String> valuesOfColumns;
     // only for inner page
     private short childrenRecord;
-
+    private DataType data;
     public Record(){
+        data = new DataType();
     }
 
 
@@ -34,10 +35,46 @@ public class Record {
         return payLoad;
     }
 
-    public void setPayLoad(short payLoad) {
-        this.payLoad = payLoad;
+    public void calculatePayLoad() {
+        for(int i=0;i<this.getNumOfColumn();i++) {
+            String object = this.getValuesOfColumns().get(i);
+            if(this.getDataTypes().get(i)==data.nameToSerialCode("null")) {
+            }
+            if(this.getDataTypes().get(i)==data.nameToSerialCode("tinyint")) {
+                this.payLoad+= 1;
+            }
+            if(this.getDataTypes().get(i)==data.nameToSerialCode("smallint")) {
+                this.payLoad+= 2;
+            }
+            if(this.getDataTypes().get(i)==data.nameToSerialCode("int")) {
+                this.payLoad+= 4;
+            }
+            if(this.getDataTypes().get(i)==data.nameToSerialCode("bigint")) {
+                this.payLoad+= 8;
+            }
+            if(this.getDataTypes().get(i)==data.nameToSerialCode("float")) {
+                this.payLoad+= 4;
+            }
+            if(this.getDataTypes().get(i)==data.nameToSerialCode("double")) {
+                this.payLoad+= 8;
+            }
+            if(this.getDataTypes().get(i)==data.nameToSerialCode("datetime")) {
+                this.payLoad+= 8;
+            }
+            if(this.getDataTypes().get(i)==data.nameToSerialCode("date")) {
+                this.payLoad+= 8;
+            }
+            if(this.getDataTypes().get(i)>data.nameToSerialCode("text")){
+                byte length = (byte) (this.getDataTypes().get(i) - data.nameToSerialCode("text"));
+                this.payLoad+= length;
+            }
+        }
+        this.payLoad+= 1;
     }
 
+    public void setPayLoad(short payLoad ) {
+        this.payLoad = payLoad ;
+    }
     public Integer getRowId() {
         return rowId;
     }
