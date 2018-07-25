ad46016b50962dcf3404a6ccb0fef92b44885113
liyooo <liliuyoyo@gmail.com>
2018-07-25 15:11:49 -0500

    add 'getColumns' method in General class

--- PeppaPigDataSystem/src/databaseAPI/General.java
+++ PeppaPigDataSystem/src/databaseAPI/General.java
@@ -1,24 +1,58 @@
 package databaseAPI;
 
+import Common.Column;
 import Common.Constants;
+import Common.DataType;
 import fileSystem.Record;
 import fileSystem.Table;
 
+import javax.xml.crypto.Data;
+import java.util.ArrayList;
 import java.util.List;
 
 public class General {
-    public static boolean checkTableExists(String tableName){
+    public static boolean checkTableExists(String tableName) {
         Table davisTable = new Table(Constants.SYSTEM_TABLES_PATH);
         List<Record> records = davisTable.getAllRecord();
-        if(records == null){
+        if (records == null) {
             return false;
-        }else{
-            for(Record r:records){
-                if(tableName.equals(r.getValuesOfColumns().get(1))){
+        } else {
+            for (Record r : records) {
+                if (tableName.equals(r.getValuesOfColumns().get(1))) {
                     return true;
                 }
             }
         }
         return false;
+    }
+
+    public List<Column> getColumns(String tableName) {
+
+        List<Column> columns = new ArrayList<>();
+
+        Table t = new Table(Constants.SYSTEM_COLUMNS_PATH);
+        List<Record> allRecords = t.getAllRecord();
+
+        for (Record r : allRecords) {
+            if (r.getValuesOfColumns().get(0) == tableName){
+                String columnName = r.getValuesOfColumns().get(1);
+                DataType dataType = new DataType(r.getValuesOfColumns().get(2));
+                boolean is_primary;
+                if(r.getValuesOfColumns().get(5).equals("1")){
+                    is_primary = true;
+                }else {
+                    is_primary = false;
+                }
+                boolean is_nullable;
+                if(r.getValuesOfColumns().get(4).equals("1")){
+                    is_nullable = true;
+                }else{
+                    is_nullable = false;
+                }
+                Column col = new Column(columnName,dataType,is_primary,is_nullable);
+                columns.add(col);
+            }
+        }
+        return  columns;
     }
 }
